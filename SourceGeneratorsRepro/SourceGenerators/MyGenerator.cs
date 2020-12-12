034266f17e8513cd9b2c2352384d4ed21c1d247e
Łukasz <lukasz.szafirski@wp.pl>
2020-12-13 00:26:06 +0100

    initial vommit

---
+++ SourceGeneratorsRepro/SourceGenerators/MyGenerator.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace SourceGenerators
+{
+    [Generator]
+    public class MyGenerator : ISourceGenerator
+    {
+        private readonly string source = @"
+using System;
+namespace SourceGeneratorsRepro
+{
+    public static class HelloWorld
+    {
+        public static void SayHello() 
+        {
+            Console.WriteLine(""Hello from generated code!"");
+            Console.WriteLine(""The following syntax trees existed in the compilation that created this program:"");
+        }
+    }
+}
+";
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            context.AddSource("HelloWorld", source);
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+    }
+}
---
+++ SourceGeneratorsRepro/SourceGeneratorsRepro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SourceGeneratorsRepro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
